dea6325ef8ec3fb24af3324c20c2eab16b28fca3
Paulo Holanda <paulorfholanda@gmail.com>
2024-07-21 12:57:39 -0300

    initial_commit

---
+++ src/main/java/enxada/TesteRecrutamento/Home.java
@@ -0,0 +1,17 @@
+package enxada.TesteRecrutamento;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Home extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
